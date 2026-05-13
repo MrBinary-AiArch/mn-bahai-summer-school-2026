8ce9db6326c906ae512ba2093bf11535c4dd1d55
Roshan <roshan@local.hub>
2026-05-13 18:18:26 -0500

    Integrate Cvent registration link (https://cvent.me/rxq051) site-wide

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         savethedate: resolve(__dirname, 'savethedate.html'),
+        registration: resolve(__dirname, 'registration.html'),
+        aboutus: resolve(__dirname, 'about-us.html'),
+        contactus: resolve(__dirname, 'contact-us.html'),
+        lodging: resolve(__dirname, 'lodging.html'),
+        schedule: resolve(__dirname, 'schedule.html'),
+        theme: resolve(__dirname, 'theme.html'),
       },
     },
   },
